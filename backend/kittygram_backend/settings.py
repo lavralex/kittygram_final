dc180fb2ebf66dfe4558c597ebf28c09ce20d53d
Александр Лаврушенков <lavralex2015@yandex.ru>
2023-09-19 01:42:19 +0300

    commit5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.getenv('KITTYGRAM_ALLOWED_HOSTS').split(', ')
+ALLOWED_HOSTS = os.getenv('KITTYGRAM_ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
